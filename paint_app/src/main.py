66c83df1ef90e1f3b97f85077b0c5b4a7e417a75
Ali Sirvanovitsj Ismailov <alisi@uia.no>
2024-11-20 22:27:41 +0100

    fixed brush size and color

--- paint_app/src/main.py
+++ paint_app/src/main.py
@@ -1,5 +1,11 @@
 from kivy.app import App
 from kivy.config import Config
+from kivy.lang import Builder
+from paint_widget import PaintWidget
+from kivy.uix.popup import Popup
+from kivy.uix.colorpicker import ColorPicker
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 # Set window size
 Config.set('graphics', 'width', '1200')
@@ -7,7 +13,35 @@ Config.set('graphics', 'height', '800')
 
 class PaintApp(App):
     def build(self):
-        return super().build()
+        self.color_popup = None  # Store popup reference
+        return Builder.load_file('paint_app/resources/paint.kv')
+    
+    def show_color_picker(self):
+        content = BoxLayout(orientation='vertical')
+        color_picker = ColorPicker()
+        content.add_widget(color_picker)
+        
+        # Add confirm button
+        confirm_button = Button(
+            text='Select Color',
+            size_hint_y=None,
+            height='48dp'
+        )
+        content.add_widget(confirm_button)
+        
+        # Create and store popup reference
+        self.color_popup = Popup(
+            title='Choose Color',
+            content=content,
+            size_hint=(0.8, 0.9)
+        )
+        
+        def on_color_select(instance):
+            self.root.ids.paint_widget.set_color(color_picker.color)
+            self.color_popup.dismiss()
+            
+        confirm_button.bind(on_release=on_color_select)
+        self.color_popup.open()
 
 if __name__ == '__main__':
     PaintApp().run()
--- paint_app/src/paint_widget.py
+++ paint_app/src/paint_widget.py
@@ -1,11 +1,16 @@
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Line
+from kivy.properties import ColorProperty, NumericProperty
 
 class PaintWidget(Widget):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.current_color = (0, 0, 0, 1)  # Default black
-        self.line_width = 2
+    current_color = ColorProperty([0, 0, 0, 1])  # Changed to ColorProperty
+    line_width = NumericProperty(2)  # Changed to NumericProperty
+
+    def set_color(self, color):
+        self.current_color = color
+        
+    def set_line_width(self, width):
+        self.line_width = width
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
